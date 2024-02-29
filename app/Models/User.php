8366daaf655cb4116b46595dcb3796585245ec28
Houssem Amri <house.mail.md@gmail.com>
2024-02-29 11:44:27 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function tester() {
+        grtinm()
+              }
+
     /**
      * The attributes that should be hidden for serialization.
      *
